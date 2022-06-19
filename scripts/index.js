be18791898852b43a0ddcb1a7335a18c1cd9f3b0
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-06-19 19:14:04 +0200

    update: close mobile navbar when click outside

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,30 @@
-//Function espression to select elements
-const selectElement = (s) => document.querySelector(s);
+const closeIcon = document.getElementById("closeIcon");
+const navbarMenu = document.getElementById("navbar-menu");
+const navList = document.getElementById("navList");
+const navListHalfEmpty = document.getElementById("navListHalfEmpty");
 
-//Open menu on click
-selectElement("#navbar-menu").addEventListener("click", () => {
-  selectElement(".nav-list").classList.add("active");
+// Open menu on click
+navbarMenu.addEventListener("click", () => {
+  navList.classList.add("active");
+  navListHalfEmpty.classList.add("is-left");
 });
 
-//Close menu on click
-selectElement(".close").addEventListener("click", () => {
-  selectElement(".nav-list").classList.remove("active");
-  // selectElement("#empty-div").classList.remove("active");
+// Close menu on click
+closeIcon.addEventListener("click", () => {
+  navList.classList.remove("active");
+  navListHalfEmpty.classList.remove("is-left");
 });
 
-// const ulEl = document.querySelector(".nav-list");
-// if (ulEl.classList.contains(active)) {
-//   console.log("attivo");
-//   document.addEventListener("click", ({ target }) => {
-//     if (!target.matches(".nav-list, .nav-list *")) {
-//       selectElement(".nav-list").classList.remove("active");
-//     }
-//   });
-// }
+// Close menu on click
+navListHalfEmpty.addEventListener("click", () => {
+  navList.classList.remove("active");
+  navListHalfEmpty.classList.remove("is-left");
+});
+
+// Close menu on click outside navList
+// window.addEventListener("click", function (e) {
+//   if (!navList.contains(e.target)) {
+//      navList.classList.remove("active");
+
+//   }
+// });
