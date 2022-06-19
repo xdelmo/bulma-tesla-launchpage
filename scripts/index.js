bde9539972b0108ced1069b54a72b414553a8d2b
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-06-19 12:44:33 +0200

    fix: Safari compatibilty issue

--- scripts/index.js
+++ scripts/index.js
@@ -9,4 +9,15 @@ selectElement("#navbar-menu").addEventListener("click", () => {
 //Close menu on click
 selectElement(".close").addEventListener("click", () => {
   selectElement(".nav-list").classList.remove("active");
+  // selectElement("#empty-div").classList.remove("active");
 });
+
+// const ulEl = document.querySelector(".nav-list");
+// if (ulEl.classList.contains(active)) {
+//   console.log("attivo");
+//   document.addEventListener("click", ({ target }) => {
+//     if (!target.matches(".nav-list, .nav-list *")) {
+//       selectElement(".nav-list").classList.remove("active");
+//     }
+//   });
+// }
